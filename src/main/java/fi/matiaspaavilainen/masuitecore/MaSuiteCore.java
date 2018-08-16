4b8d99e49125bc8f4ab05d6c1d21db272aec56ea
Masa <masa@simasasolutions.com>
2018-08-16 17:11:41 +0300

    Initial commit

---
+++ src/main/java/fi/matiaspaavilainen/masuitecore/MaSuiteCore.java
@@ -0,0 +1,11 @@
+package fi.matiaspaavilainen.masuitecore;
+
+import net.md_5.bungee.api.plugin.Plugin;
+import org.bstats.bungeecord.Metrics;
+
+public class MaSuiteCore extends Plugin {
+    @Override
+    public void onEnable() {
+        Metrics metrics = new Metrics(this);
+    }
+}
