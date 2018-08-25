f327526658b19406c562776e90b1f1603890ee7d
Matias Paavilainen <paavilainen.matias@gmail.com>
2018-08-25 20:50:06 +0300

    Added better prettier message sending

--- src/main/java/fi/matiaspaavilainen/masuitecore/chat/Formator.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/chat/Formator.java
@@ -1,10 +1,16 @@
 package fi.matiaspaavilainen.masuitecore.chat;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class Formator {
 
     public String colorize(String string){
         return ChatColor.translateAlternateColorCodes('&', string);
+    }
+
+    public void sendMessage(ProxiedPlayer p, String message){
+        p.sendMessage(new TextComponent(colorize(message)));
     }
 }
