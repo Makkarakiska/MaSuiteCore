6537fdfcf1e9e588daf0dc2e37d02ec37890825f
Masa <masa@simasasolutions.com>
2018-12-29 20:44:53 +0200

    Added configuration

---
+++ src/main/java/fi/matiaspaavilainen/masuitecore/core/configuration/BukkitConfiguration.java
@@ -0,0 +1,77 @@
+package fi.matiaspaavilainen.masuitecore.core.configuration;
+
+import com.google.common.io.ByteStreams;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+
+public class BukkitConfiguration {
+
+    private JavaPlugin plugin;
+
+    /**
+     * Constructor for BukkitConfiguration
+     */
+    public BukkitConfiguration(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
+     * Loads configuration file
+     *
+     * @param folder module folder
+     * @param config file name
+     * @return configuration file to use
+     **/
+    public FileConfiguration load(String folder, String config) {
+        FileConfiguration configuration = null;
+        File f = null;
+        if (folder != null) {
+            f = new File("plugins/MaSuite/" + folder);
+        } else {
+            f = new File("plugins/MaSuite");
+        }
+        try {
+            configuration = new YamlConfiguration();
+            configuration.load(new File(f, config));
+            return configuration;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Creates configuration file
+     *
+     * @param folder module folder
+     * @param config file name
+     */
+    public void create(String folder, String config) {
+        File f = null;
+        if (folder != null) {
+            f = new File("plugins/MaSuite/" + folder);
+        } else {
+            f = new File("plugins/MaSuite");
+        }
+
+        if (!f.exists()) {
+            f.mkdir();
+        }
+        File configFile = new File(f, config);
+        if (!configFile.exists()) {
+            try {
+                configFile.createNewFile();
+                try (InputStream is = plugin.getResource(config);
+                     OutputStream os = new FileOutputStream(configFile)) {
+                    ByteStreams.copy(is, os);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to create configuration file", e);
+            }
+        }
+    }
+
+}
--- src/main/java/fi/matiaspaavilainen/masuitecore/core/configuration/BungeeConfiguration.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/core/configuration/BungeeConfiguration.java
@@ -1,22 +1,44 @@
-package fi.matiaspaavilainen.masuitecore.config;
+package fi.matiaspaavilainen.masuitecore.core.configuration;
 
 import com.google.common.io.ByteStreams;
-import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
 import java.io.*;
 
-public class Configuration {
+public class BungeeConfiguration {
+
+    private Configuration configuration;
+    private File file;
+
+    /**
+     * An empty constructor for BungeeConfiguration
+     */
+    public BungeeConfiguration() {
+
+    }
+
+    /**
+     * Constructor for BungeeConfiguration
+     *
+     * @param configuration {@link Configuration} to use
+     * @param file          {@link File} to use
+     */
+    public BungeeConfiguration(Configuration configuration, File file) {
+        this.configuration = configuration;
+        this.file = file;
+    }
 
     /**
      * Loads configuration file
+     *
      * @param folder module folder
      * @param config file name
      * @return configuration file to use
      */
-    public net.md_5.bungee.config.Configuration load(String folder, String config) {
-        net.md_5.bungee.config.Configuration configuration = null;
+    public Configuration load(String folder, String config) {
+        Configuration configuration = null;
         File f = null;
         if (folder != null) {
             f = new File("plugins/MaSuite/" + folder);
@@ -27,36 +49,30 @@ public class Configuration {
             configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(f, config));
             return configuration;
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
         return null;
     }
-
-
     /**
-     * Loads configuration file
-     * @param config file name
-     * @return configuration file to use
+     * Add default value to config file
+     *
+     * @param path  path to value
+     * @param value value
      */
-    @Deprecated
-    public net.md_5.bungee.config.Configuration load(String config) {
-        net.md_5.bungee.config.Configuration configuration = null;
-        try {
-            configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File("plugins/MaSuite/", config));
-            return configuration;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+    public void addDefault(String path, Object value) {
+        if (this.configuration.get(path) == null) {
+            this.configuration.set(path, value);
+            new BungeeConfiguration().save(this.configuration, this.file.getName());
         }
-        return null;
     }
 
     /**
      * Creates configuration file
-     * @param p plugin to use
+     *
      * @param folder module folder
      * @param config file name
      */
-    public void create(Plugin p, String folder, String config) {
+    public void create(String folder, String config) {
         File f = null;
         if (folder != null) {
             f = new File("plugins/MaSuite/" + folder);
@@ -71,7 +87,7 @@ public class Configuration {
         if (!configFile.exists()) {
             try {
                 configFile.createNewFile();
-                try (InputStream is = p.getResourceAsStream(config);
+                try (InputStream is = getClass().getResourceAsStream(config);
                      OutputStream os = new FileOutputStream(configFile)) {
                     ByteStreams.copy(is, os);
                 }
@@ -83,22 +99,23 @@ public class Configuration {
 
     /**
      * Creates configuration file
-     * @param p plugin to use
+     *
      * @param config file name
      */
-    public void create(Plugin p, String config) {
+    public void create(String config) {
         File f = new File("plugins/MaSuite");
         if (!f.exists()) {
             f.mkdir();
         }
         File configFile = new File(f, config);
-        configChecker(p, config, configFile);
+        configChecker(configFile);
     }
 
     /**
      * Saves the configuration file
+     *
      * @param config file
-     * @param file file name
+     * @param file   file name
      */
     public void save(net.md_5.bungee.config.Configuration config, String file) {
         try {
@@ -108,17 +125,39 @@ public class Configuration {
         }
     }
 
-    private void configChecker(Plugin p, String config, File configFile) {
-        if (!configFile.exists()) {
+    /**
+     * Check if configuration exists
+     *
+     * @param config configuration to check
+     */
+    private void configChecker(File config) {
+        if (!config.exists()) {
             try {
-                configFile.createNewFile();
-                try (InputStream is = p.getResourceAsStream(config);
-                     OutputStream os = new FileOutputStream(configFile)) {
+                config.createNewFile();
+                try (InputStream is = getClass().getResourceAsStream(config.getName());
+                     OutputStream os = new FileOutputStream(config)) {
                     ByteStreams.copy(is, os);
                 }
             } catch (IOException e) {
                 throw new RuntimeException("Unable to create configuration file", e);
             }
         }
+    }
+
+
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
     }
 }
