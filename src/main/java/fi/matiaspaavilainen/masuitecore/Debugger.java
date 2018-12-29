7223c36d9d4e9f5b14613111fe712e22a1a9a2f2
Masa <masa@simasasolutions.com>
2018-12-29 20:51:09 +0200

    Moved to BungeeConfiguration

--- src/main/java/fi/matiaspaavilainen/masuitecore/Debugger.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/Debugger.java
@@ -1,12 +1,12 @@
 package fi.matiaspaavilainen.masuitecore;
 
 import fi.matiaspaavilainen.masuitecore.chat.Formator;
-import fi.matiaspaavilainen.masuitecore.config.Configuration;
+import fi.matiaspaavilainen.masuitecore.core.configuration.BungeeConfiguration;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class Debugger {
 
-    private Configuration configuration = new Configuration();
+    private BungeeConfiguration configuration = new BungeeConfiguration();
     private Formator formator = new Formator();
     public void sendMessage(String message){
         if(configuration.load(null,"config.yml").getBoolean("debug")){
