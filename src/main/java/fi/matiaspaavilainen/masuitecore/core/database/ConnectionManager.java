8efd4969f9448dd59ad4a84b85f8b4706d07cfdb
Masa <masa@simasasolutions.com>
2018-12-30 13:31:29 +0200

    Added params and documented

--- src/main/java/fi/matiaspaavilainen/masuitecore/core/database/ConnectionManager.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/core/database/ConnectionManager.java
@@ -4,15 +4,37 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    public static Database db = new Database();
+    private String tablePrefix;
+    private String address;
+    private int port;
+    private String database;
+    private String username;
+    private String password;
+
+    public static Database db = null;
 
     /**
      * An empty constructor for ConnectionManager
+     *
+     * @param tablePrefix prefix of all MaSuite tables
+     * @param address     address of the MySQL server
+     * @param port        port of the MySQL server
+     * @param database    database of the MySQL server
+     * @param username    username of the MySQl server
+     * @param password    password of the MySQL server
      */
-    public ConnectionManager() {}
+    public ConnectionManager(String tablePrefix, String address, int port, String database, String username, String password) {
+        this.tablePrefix = tablePrefix;
+        this.address = address;
+        this.port = port;
+        this.database = database;
+        this.username = username;
+        this.password = password;
+    }
 
     /**
      * Get database
+     *
      * @return {@link Database}
      */
     public Database getDatabase() {
@@ -23,6 +45,7 @@ public class ConnectionManager {
      * Connect to {@link Database}
      */
     public void connect() {
+        db = new Database(this.tablePrefix, this.address, this.port, this.database, this.username, this.password);
         db.connect();
     }
 
@@ -38,4 +61,5 @@ public class ConnectionManager {
             e.printStackTrace();
         }
     }
+
 }
