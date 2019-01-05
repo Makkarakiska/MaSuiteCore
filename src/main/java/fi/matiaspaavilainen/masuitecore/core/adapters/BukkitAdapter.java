2fd0ffc9c66578b2b413b97d3d4519f7ffed8ff0
Masa <masa@simasasolutions.com>
2019-01-05 12:03:32 +0200

    Documented

--- src/main/java/fi/matiaspaavilainen/masuitecore/core/adapters/BukkitAdapter.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/core/adapters/BukkitAdapter.java
@@ -6,6 +6,12 @@ import org.bukkit.Bukkit;
 
 public class BukkitAdapter {
 
+    /**
+     * Get {@link org.bukkit.Location} from @{@link Location}
+     *
+     * @param loc location to use
+     * @return new {@link org.bukkit.Location}
+     */
     public static org.bukkit.Location adapt(Location loc) {
         org.bukkit.Location location;
         if (loc.getPitch() != null) {
@@ -16,6 +22,12 @@ public class BukkitAdapter {
         return location;
     }
 
+    /**
+     * Get @{@link Location} from {@link org.bukkit.Location}
+     *
+     * @param loc location to use
+     * @return new @{@link Location}
+     */
     public static Location adapt(org.bukkit.Location loc) {
         return new Location(loc.getWorld().getName(), loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
     }
