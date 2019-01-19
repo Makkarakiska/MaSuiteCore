7d4f307f11bb0adcf3a35508c1bf9e760bda7ad7
Masa <masa@simasasolutions.com>
2019-01-19 10:58:32 +0200

    Moved dateformator from Date class

--- src/main/java/fi/matiaspaavilainen/masuitecore/bukkit/chat/Formator.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/bukkit/chat/Formator.java
@@ -5,10 +5,14 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Formator {
 
     /**
      * Colorize String
+     *
      * @param string to color
      * @return colorized string
      */
@@ -18,7 +22,8 @@ public class Formator {
 
     /**
      * Sends message to {@link Player}
-     * @param p target player
+     *
+     * @param p       target player
      * @param message message to send
      */
     public void sendMessage(Player p, String message) {
@@ -27,10 +32,22 @@ public class Formator {
 
     /**
      * Sends message to {@link CommandSender}
-     * @param cs target CommandSender
+     *
+     * @param cs      target CommandSender
      * @param message message to send
      */
     public void sendMessage(CommandSender cs, String message) {
         cs.spigot().sendMessage(TextComponent.fromLegacyText(colorize(message)));
+    }
+
+    /**
+     * Create formatted timestamp
+     *
+     * @param date to format
+     * @return formatted date
+     */
+    public String getDate(Date date) {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        return format.format(date);
     }
 }
--- src/main/java/fi/matiaspaavilainen/masuitecore/bungee/chat/Formator.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/bungee/chat/Formator.java
@@ -3,10 +3,14 @@ package fi.matiaspaavilainen.masuitecore.bungee.chat;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Formator {
 
     /**
      * Colorize String
+     *
      * @param string to color
      * @return colorized string
      */
@@ -16,10 +20,22 @@ public class Formator {
 
     /**
      * Sends message to {@link ProxiedPlayer}
-     * @param p target player
+     *
+     * @param p       target player
      * @param message message to send
      */
     public void sendMessage(ProxiedPlayer p, String message) {
         p.sendMessage(MDChat.getMessageFromString(colorize(message)));
+    }
+
+    /**
+     * Create formatted timestamp
+     *
+     * @param date to format
+     * @return formatted date
+     */
+    public String getDate(Date date) {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        return format.format(date);
     }
 }
