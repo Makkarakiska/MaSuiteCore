39236aae0b47ba64eae935f28bc219340dce3d45
Masa <masa@simasasolutions.com>
2019-01-21 11:26:55 +0200

    Fixed NPE if the plugin was not found!

--- src/main/java/fi/matiaspaavilainen/masuitecore/bukkit/commands/MaSuiteCommand.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/bukkit/commands/MaSuiteCommand.java
@@ -25,19 +25,19 @@ public class MaSuiteCommand implements CommandExecutor {
         PluginManager pm = plugin.getServer().getPluginManager();
 
         formator.sendMessage(sender, " &8&lMaSuiteCore &9v" + plugin.getDescription().getVersion());
-        if (pm.getPlugin("MaSuiteHomes").isEnabled()) {
+        if (pm.getPlugin("MaSuiteHomes") != null && pm.getPlugin("MaSuiteHomes").isEnabled()) {
             formator.sendMessage(sender, " &8&lMaSuiteHomes &9v" + pm.getPlugin("MaSuiteHomes").getDescription().getVersion());
         }
-        if (pm.getPlugin("MaSuiteChat").isEnabled()) {
+        if (pm.getPlugin("MaSuiteChat") != null && pm.getPlugin("MaSuiteChat").isEnabled()) {
             formator.sendMessage(sender, " &8&lMaSuiteChat &9v" + pm.getPlugin("MaSuiteChat").getDescription().getVersion());
         }
-        if (pm.getPlugin("MaSuiteWarps").isEnabled()) {
+        if (pm.getPlugin("MaSuiteWarps") != null && pm.getPlugin("MaSuiteWarps").isEnabled()) {
             formator.sendMessage(sender, " &8&lMaSuiteWarps &9v" + pm.getPlugin("MaSuiteWarps").getDescription().getVersion());
         }
-        if (pm.getPlugin("MaSuiteTeleports").isEnabled()) {
+        if (pm.getPlugin("MaSuiteTeleports") != null && pm.getPlugin("MaSuiteTeleports").isEnabled()) {
             formator.sendMessage(sender, " &8&lMaSuiteTeleports &9v" + pm.getPlugin("MaSuiteTeleports").getDescription().getVersion());
         }
-        if (pm.getPlugin("MaSuitePortals").isEnabled()) {
+        if (pm.getPlugin("MaSuitePortals") != null && pm.getPlugin("MaSuitePortals").isEnabled()) {
             formator.sendMessage(sender, " &8&lMaSuitePortals &9v" + pm.getPlugin("MaSuitePortals").getDescription().getVersion());
         }
         formator.sendMessage(sender, " &8&lSupport link &9https://discord.gg/sZZG6Jq");
