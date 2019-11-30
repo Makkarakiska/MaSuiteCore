51eac20dc37765b8c9269abddb2359e71a36fc60
Matias Paavilainen <matias@paavilainen.net>
2019-11-30 20:03:46 +0200

    Added embeddable

--- src/main/java/fi/matiaspaavilainen/masuitecore/core/objects/Location.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/core/objects/Location.java
@@ -2,6 +2,12 @@ package fi.matiaspaavilainen.masuitecore.core.objects;
 
 import net.md_5.bungee.api.config.ServerInfo;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Access(AccessType.PROPERTY)
 public class Location {
 
     private ServerInfo server;
