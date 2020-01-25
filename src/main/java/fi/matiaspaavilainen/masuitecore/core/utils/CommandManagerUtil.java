a304582d3494a9deb3929290c815c834a54ce6b7
Matias Paavilainen <matias@paavilainen.net>
2020-01-25 19:19:44 +0200

    Added cooldown condition

--- src/main/java/fi/matiaspaavilainen/masuitecore/core/utils/CommandManagerUtil.java
+++ src/main/java/fi/matiaspaavilainen/masuitecore/core/utils/CommandManagerUtil.java
@@ -1,12 +1,16 @@
 package fi.matiaspaavilainen.masuitecore.core.utils;
 
+import co.aikar.commands.ConditionFailedException;
 import co.aikar.commands.InvalidCommandArgument;
 import co.aikar.commands.PaperCommandManager;
+import fi.matiaspaavilainen.masuitecore.bukkit.BukkitCooldownManager;
 import fi.matiaspaavilainen.masuitecore.bukkit.MaSuiteCore;
 import fi.matiaspaavilainen.masuitecore.core.models.MaSuitePlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+
+import java.util.UUID;
 
 public class CommandManagerUtil {
 
@@ -18,6 +22,28 @@ public class CommandManagerUtil {
     public static void registerMaSuitePlayerCommandCompletion(PaperCommandManager manager) {
         manager.getCommandCompletions().registerCompletion("masuite_players", c -> MaSuiteCore.onlinePlayers);
     }
+
+
+    /**
+     * Register a cooldown condition for plugins to use
+     *
+     * @param manager manager to use
+     */
+    public void registerCooldownCondition(PaperCommandManager manager) {
+        manager.getCommandConditions().addCondition("cooldown", c -> {
+            BukkitCooldownManager cooldownManager = MaSuiteCore.cooldownManager;
+            UUID uuid = c.getIssuer().getUniqueId();
+
+            String cooldownType = c.getConfigValue("type", "");
+            String byPassPermission = c.getConfigValue("bypass", "masuitecore.cooldown.bypass");
+
+            if (!c.getIssuer().hasPermission(byPassPermission) && cooldownManager.hasCooldown(cooldownType, uuid)) {
+                // TODO: Switch to config
+                throw new ConditionFailedException("You are in cooldown.");
+            }
+        });
+    }
+
 
     /**
      * Registers {@link Location} command context for {@link PaperCommandManager}
@@ -56,4 +82,6 @@ public class CommandManagerUtil {
         }
         return coordinate;
     }
+
+
 }
