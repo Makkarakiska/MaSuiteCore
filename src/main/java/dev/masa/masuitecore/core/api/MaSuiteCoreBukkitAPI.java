6ed403c9da1d3f93052ba24c91ca1af2578c3a5e
Matias Paavilainen <matias@paavilainen.net>
2020-02-07 10:33:58 +0200

    Added Bukkit API

---
+++ src/main/java/dev/masa/masuitecore/core/api/MaSuiteCoreBukkitAPI.java
@@ -0,0 +1,33 @@
+package dev.masa.masuitecore.core.api;
+
+import dev.masa.masuitecore.bukkit.MaSuiteCore;
+import dev.masa.masuitecore.core.services.CooldownService;
+import dev.masa.masuitecore.core.services.WarmupService;
+
+public class MaSuiteCoreBukkitAPI {
+
+    /**
+     * Get the MaSuiteCore's Bukkit instance
+     * @return returns MaSuiteCore Bukkit instance
+     */
+    public static MaSuiteCore getCore() {
+        return MaSuiteCore.getInstance();
+    }
+
+    /**
+     * Get the warmup service
+     * @return returns warmup service
+     */
+    public WarmupService getWarmupService() {
+        return MaSuiteCore.warmupService;
+    }
+
+
+    /**
+     * Get the cooldown service
+     * @return returns cooldown service
+     */
+    public CooldownService getCooldownService() {
+        return MaSuiteCore.cooldownService;
+    }
+}
