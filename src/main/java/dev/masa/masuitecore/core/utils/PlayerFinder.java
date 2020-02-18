002ecddb90227410f195e4bc6eb286e7c09f9b2a
Matias Paavilainen <matias@paavilainen.net>
2020-02-18 19:11:13 +0200

    Rewrote PlayerFInder with a method from Bungee API

--- src/main/java/dev/masa/masuitecore/core/utils/PlayerFinder.java
+++ src/main/java/dev/masa/masuitecore/core/utils/PlayerFinder.java
@@ -6,18 +6,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 public class PlayerFinder {
 
     public ProxiedPlayer get(String name) {
-        try {
-            if (ProxyServer.getInstance().getPlayers().stream().anyMatch(proxiedPlayer -> proxiedPlayer.getName().toLowerCase().startsWith(name.toLowerCase()))) {
-                return ProxyServer.getInstance().getPlayers().stream().filter(proxiedPlayer ->
-                        proxiedPlayer.getName().toLowerCase().startsWith(name.toLowerCase()) ||
-                                proxiedPlayer.getName().equalsIgnoreCase(name))
-                        .findFirst().get();
-            } else {
-                return null;
-            }
-        } catch (Exception ignored) {
-        }
-        return null;
+        return ProxyServer.getInstance().matchPlayer(name).stream().findFirst().orElse(null);
     }
 }
 
