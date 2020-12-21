2241660f43ce12c6f357c493ce05628eea8cf638
Matias Paavilainen <matias@paavilainen.net>
2020-12-21 19:05:51 +0200

    Removed unused stuff

--- src/main/java/dev/masa/masuitecore/bungee/Utils.java
+++ src/main/java/dev/masa/masuitecore/bungee/Utils.java
@@ -1,9 +1,8 @@
 package dev.masa.masuitecore.bungee;
 
 import dev.masa.masuitecore.bungee.chat.Formator;
-import dev.masa.masuitecore.core.channels.BungeePluginChannel;
-import dev.masa.masuitecore.core.objects.Location;
-import dev.masa.masuitecore.core.configuration.BungeeConfiguration;
+import dev.masa.masuitecore.common.channels.BungeePluginChannel;
+import dev.masa.masuitecore.common.objects.Location;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -13,8 +12,7 @@ import java.util.UUID;
 
 public class Utils {
 
-    private BungeeConfiguration config = new BungeeConfiguration();
-    private Formator formator = new Formator();
+    private final Formator formator = new Formator();
 
 
     /**
@@ -36,33 +34,11 @@ public class Utils {
      */
     public boolean isOnline(ProxiedPlayer target, ProxiedPlayer sender) {
         if (target == null) {
-            formator.sendMessage(sender, config.load(null, "messages.yml").getString("player-not-online"));
+
+            formator.sendMessage(sender, MaSuiteCore.getInstance().getMessages().getPlayerNotOnline());
             return false;
         }
         return true;
-    }
-
-    /**
-     * Send debug message to console
-     *
-     * @param message the message to send
-     */
-    public void sendMessage(String message) {
-        if (config.load(null, "config.yml").getBoolean("debug")) {
-            System.out.println(formator.colorize(message));
-        }
-    }
-
-    /**
-     * Send debug message to {@link ProxiedPlayer}
-     *
-     * @param player  player to send the message
-     * @param message the message to send
-     */
-    public void sendMessage(ProxiedPlayer player, String message) {
-        if (config.load(null, "config.yml").getBoolean("debug")) {
-            formator.sendMessage(player, message);
-        }
     }
 
     /**
